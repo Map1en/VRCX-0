59f1a0da74079bce93107e12cd18a286a243405b
pa <maplenagisa@gmail.com>
2026-04-18 23:23:57 +0900

    fix: dialog styles

--- src/components/dialogs/EntityDialogScaffold.jsx
+++ src/components/dialogs/EntityDialogScaffold.jsx
@@ -19,7 +19,7 @@ function EntityDialogScaffold({ className, children }) {
     return (
         <div
             className={cn(
-                'flex min-h-0 w-full min-w-0 flex-1 flex-col gap-4 md:w-[65rem]',
+                'flex min-h-0 w-full min-w-0 flex-1 flex-col gap-4',
                 className
             )}
         >
