30e17d1da73247009fd6c861166048a9f58abeb3
pa <maplenagisa@gmail.com>
2026-04-23 11:03:15 +0900

    refactor

--- src-tauri/src/api/app.rs
+++ src-tauri/src/api/app.rs
@@ -13,27 +13,4 @@ pub mod shell;
 pub mod updates;
 pub mod window;
 
-#[allow(unused_imports)]
-pub use calendar::*;
-#[allow(unused_imports)]
-pub use clipboard::*;
-#[allow(unused_imports)]
-pub use game::*;
-#[allow(unused_imports)]
-pub use ipc_commands::*;
-#[allow(unused_imports)]
-pub use media::*;
-#[allow(unused_imports)]
-pub use moderation::*;
-#[allow(unused_imports)]
-pub use paths::*;
-#[allow(unused_imports)]
-pub use registry::*;
-#[allow(unused_imports)]
-pub use screenshots::*;
-#[allow(unused_imports)]
-pub use shell::*;
-#[allow(unused_imports)]
-pub use updates::*;
-#[allow(unused_imports)]
-pub use window::*;
+pub use paths::app__get_vrchat_cache_location;
