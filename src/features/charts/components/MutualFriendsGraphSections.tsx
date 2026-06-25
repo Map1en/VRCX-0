b79807ba3c564cb51f8cf0d4ae74a0d54370811e
flower_elf <47104993+flower-elf@users.noreply.github.com>
2026-06-25 19:55:27 +0800

    add missing key again

--- src/features/charts/components/MutualFriendsGraphSections.tsx
+++ src/features/charts/components/MutualFriendsGraphSections.tsx
@@ -292,7 +292,7 @@ function MutualFriendsSettingsSheet({
                     </div>
                     <div className="text-muted-foreground text-xs">
                         {t('view.charts.label.hidden_nodes')} {excludedCount}.
-                        Visible nodes: {nodeCount}. Visible links: {edgeCount}.
+                        {t('view.charts.label.visible_nodes')} {nodeCount}. {t('view.charts.label.visible_links')} {edgeCount}.
                     </div>
                     <Button
                         type="button"
