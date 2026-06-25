e6c29e05289ae920c9b4386fa61a3faa42b6957f
flower_elf <47104993+flower-elf@users.noreply.github.com>
2026-06-25 20:38:02 +0800

    fix: improve formatting of node visibility labels in MutualFriendsSettingsSheet

--- src/features/charts/components/MutualFriendsGraphSections.tsx
+++ src/features/charts/components/MutualFriendsGraphSections.tsx
@@ -291,9 +291,11 @@ function MutualFriendsSettingsSheet({
                         </p>
                     </div>
                     <div className="text-muted-foreground text-xs">
-                        {t('view.charts.label.hidden_nodes')} {excludedCount}.
-                        {t('view.charts.label.visible_nodes')} {nodeCount}.
-                        {t('view.charts.label.visible_links')} {edgeCount}.
+                        {t('view.charts.label.hidden_nodes')} {excludedCount}
+                        <br />
+                        {t('view.charts.label.visible_nodes')} {nodeCount}
+                        <br />
+                        {t('view.charts.label.visible_links')} {edgeCount}
                     </div>
                     <Button
                         type="button"
