541db63478e795364839a0bcf861fbb55186b7fa
Cyan Changes <contact@cyans.me>
2026-07-12 18:12:16 +0800

    feat: add command in devkit to manually trigger panic

--- src-tauri/src/commands/host/devkit.rs
+++ src-tauri/src/commands/host/devkit.rs
@@ -1,9 +1,26 @@
 #![allow(non_snake_case)]
+#![cfg_attr(not(feature = "devkit"), allow(unused_variables, dead_code))]
 
 use tauri::State;
 
 use crate::error::AppError;
 use crate::state::AppState;
+
+#[cfg(not(feature = "devkit"))]
+macro_rules! devkit_feature {
+    ($_:expr) => {
+        return Err(AppError::Custom(
+            "Devkit tools are unavailable in this build.".into(),
+        ));
+    };
+}
+
+#[cfg(feature = "devkit")]
+macro_rules! devkit_feature {
+    ($code:expr) => {
+        $code
+    };
+}
 
 #[tauri::command]
 #[specta::specta]
@@ -11,20 +28,24 @@ pub fn app__devkit_read_file(
     state: State<'_, AppState>,
     file_path: String,
 ) -> Result<String, AppError> {
-    #[cfg(not(feature = "devkit"))]
-    {
-        let _ = state;
-        let _ = file_path;
-        Err(AppError::Custom(
-            "Dev kit tools are unavailable in this build.".into(),
-        ))
-    }
-
-    #[cfg(feature = "devkit")]
-    {
+    devkit_feature!({
         state
             .host_file_access
             .ensure_read_allowed(&file_path, &state.paths)?;
         Ok(std::fs::read_to_string(&file_path)?)
-    }
+    })
+}
+
+#[tauri::command]
+#[specta::specta]
+pub fn app__devkit_panic(
+    state: State<'_, AppState>,
+    message: Option<String>,
+) -> Result<(), AppError> {
+    devkit_feature!({
+        panic!(
+            "{}",
+            message.as_deref().unwrap_or("manually triggered panic")
+        )
+    })
 }
